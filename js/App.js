75da3fc7e2e993c9d12685d5dc78bfd5bb1d2829
Marcin Ratajczak <maratajczak@gmail.com>
2018-12-20 18:38:34 +0100

    Update js code

--- js/App.js
+++ js/App.js
@@ -1,19 +1,11 @@
-// OGÓLNA FUNKCJA
-var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
-var myHeaders = {
-    'X-Client-Id': '3727',
-    'X-Auth-Token': 'e20f7d1aee52ff21718a986d41b31ff1'
-};
+'use strict';
 
-fetch(baseUrl + '/board', {
-        headers: myHeaders
-    })
-    .then(function (resp) {
-        return resp.json();
-    })
-    .then(function (resp) {
-        setupColumns(resp.columns);
-    });
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var prefix = 'https://cors-anywhere.herokuapp.com/';
+var myHeaders = {
+  'X-Client-Id': '3727',
+  'X-Auth-Token': 'e20f7d1aee52ff21718a986d41b31ff1'
+};
 
 function generateTemplate(name, data, basicElement) {
     var template = document.getElementById(name).innerHTML;
@@ -25,17 +17,25 @@ function generateTemplate(name, data, basicElement) {
     return element;
 }
 
-function setupColumns(columns) {
-    columns.forEach(function (column) {
-        var col = new Column(column.id, column.name);
-        board.addColumn(col);
-        setupCards(col, column.cards);
-    });
-}
+fetch(prefix + baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+  });
 
-function setupCards(col, cards) {
-    cards.forEach(function (card) {
-        var cardObj = new Card(card.id, card.name);
-        col.addCard(cardObj);
+  function setupColumns(columns) {
+    columns.forEach(function (column) {
+          var col = new Column(column.id, column.name);
+      board.addColumn(col);
+      setupCards(col, column.cards);
     });
+  }
+
+  function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
 }
--- js/Card.js
+++ js/Card.js
@@ -1,5 +1,11 @@
-// KLASA KANBAN CARD
+'use strict';
 
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var prefix = 'https://cors-anywhere.herokuapp.com/';
+var myHeaders = {
+  'X-Client-Id': '3727',
+  'X-Auth-Token': 'e20f7d1aee52ff21718a986d41b31ff1'
+};
 
 function Card(id, name) {
     var self = this;
@@ -9,26 +15,24 @@ function Card(id, name) {
     this.element = generateTemplate('card-template', { description: this.name }, 'li');
 
     this.element.querySelector('.card').addEventListener('click', function (event) {
-        event.stopPropagation();
+      event.stopPropagation();
 
-        if (event.target.classList.contains('btn-delete')) {
+      if (event.target.classList.contains('btn-delete')) {
             self.removeCard();
-        }
+      }
     });
 }
 Card.prototype = {
-    removeCard: function () {
-        var self = this;
-
-        fetch(baseUrl + '/card/' + self.id, {
-                method: 'DELETE',
-                headers: myHeaders
-            })
-            .then(function (resp) {
-                return resp.json();
-            })
-            .then(function (resp) {
-                self.element.parentNode.removeChild(this.element);
-            })
-    }
-}
+  
+  removeCard: function() {
+    var self = this;
+  
+    fetch(prefix + baseUrl + '/card/' + self.id, { method: 'DELETE', headers: myHeaders })
+      .then(function(resp) {
+        return resp.json();
+      })
+      .then(function(resp) {
+        self.element.parentNode.removeChild(this.element);
+      })
+  }
+};
