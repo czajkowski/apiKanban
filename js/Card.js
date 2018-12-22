2a79731cd7ace7aed4b0844c7e127a45e65e5b95
Allucard79 <44382180+Allucard79@users.noreply.github.com>
2018-12-22 20:32:21 +0100

    Update Card.js

--- js/Card.js
+++ js/Card.js
@@ -1,12 +1,5 @@
 'use strict';
 
-var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
-var prefix = 'https://cors-anywhere.herokuapp.com/';
-var myHeaders = {
-  'X-Client-Id': '3727',
-  'X-Auth-Token': 'e20f7d1aee52ff21718a986d41b31ff1'
-};
-
 function Card(id, name) {
     var self = this;
 
