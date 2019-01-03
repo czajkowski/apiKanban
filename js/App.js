83d545d835eb4c983220cd8286fcd92173874800
Allucard79 <44382180+Allucard79@users.noreply.github.com>
2019-01-03 22:44:07 +0100

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,42 +1,49 @@
 'use strict';
 
-const baseUrl = 'https://kodilla.com/pl/bootcamp-api';
-const prefix = 'https://cors-anywhere.herokuapp.com/';
-const myHeaders = {
-  'X-Client-Id': '3727',
-  'X-Auth-Token': 'e20f7d1aee52ff21718a986d41b31ff1'
-};
-
 function generateTemplate(name, data, basicElement) {
-    var template = document.getElementById(name).innerHTML;
-    var element = document.createElement(basicElement || 'div');
+  var template = document.getElementById(name).innerHTML;
+  var element = document.createElement(basicElement || 'div');
 
-    Mustache.parse(template);
-    element.innerHTML = Mustache.render(template, data);
+  Mustache.parse(template);
+  element.innerHTML = Mustache.render(template, data);
 
-    return element;
+  return element;
 }
 
-fetch(prefix + baseUrl + '/board', { headers: myHeaders })
-  .then(function(resp) {
-    return resp.json();
-  })
-  .then(function(resp) {
-    setupColumns(resp.columns);
-  });
+function start() {
+  var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+  var prefix = 'https://cors-anywhere.herokuapp.com/';
+  var myHeaders = {
+    'X-Client-Id': '3727',
+    'X-Auth-Token': 'e20f7d1aee52ff21718a986d41b31ff1'
+  };
+  const Column = initializeColumn(prefix, baseUrl, myHeaders);
+  const board = initializeBoard(prefix, baseUrl, myHeaders);
+  const Card = initializeCard(prefix, baseUrl, myHeaders);
+
+  fetch(prefix + baseUrl + '/board', {
+      headers: myHeaders
+    })
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (resp) {
+      setupColumns(resp.columns);
+    });
 
   function setupColumns(columns) {
     columns.forEach(function (column) {
-          var col = new Column(column.id, column.name);
+      var col = new Column(column.id, column.name);
       board.addColumn(col);
       setupCards(col, column.cards);
     });
   }
 
   function setupCards(col, cards) {
-	cards.forEach(function (card) {
-    var cardObj = new Card(card.id, card.name);
-  	col.addCard(cardObj);
-	});
+    cards.forEach(function (card) {
+      var cardObj = new Card(card.id, card.name);
+      col.addCard(cardObj);
+    });
+  }
 }
-
+start();
