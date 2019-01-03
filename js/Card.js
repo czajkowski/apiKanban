f094674a012ce433e578f64bea78f7839d3b9d8f
Allucard79 <44382180+Allucard79@users.noreply.github.com>
2019-01-03 22:45:19 +0100

    Update Card.js

--- js/Card.js
+++ js/Card.js
@@ -1,31 +1,39 @@
 'use strict';
 
-function Card(id, name) {
+function initializeCard(prefix, baseUrl, myHeaders) {
+  function Card(id, name) {
     var self = this;
 
     this.id = id;
     this.name = name || 'No name given';
-    this.element = generateTemplate('card-template', { description: this.name }, 'li');
+    this.element = generateTemplate('card-template', {
+      description: this.name
+    }, 'li');
 
     this.element.querySelector('.card').addEventListener('click', function (event) {
       event.stopPropagation();
 
       if (event.target.classList.contains('btn-delete')) {
-            self.removeCard();
+        self.removeCard();
       }
     });
-}
-Card.prototype = {
-  
-  removeCard: function() {
-    var self = this;
-  
-    fetch(prefix + baseUrl + '/card/' + self.id, { method: 'DELETE', headers: myHeaders })
-      .then(function(resp) {
-        return resp.json();
-      })
-      .then(function(resp) {
-        self.element.parentNode.removeChild(self.element);
-      })
   }
-};
+  Card.prototype = {
+
+    removeCard: function () {
+      var self = this;
+
+      fetch(prefix + baseUrl + '/card/' + self.id, {
+          method: 'DELETE',
+          headers: myHeaders
+        })
+        .then(function (resp) {
+          return resp.json();
+        })
+        .then(function (resp) {
+          self.element.parentNode.removeChild(self.element);
+        })
+    }
+  };
+  return Card;
+}
