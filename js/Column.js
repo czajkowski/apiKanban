1352c949d401fb3f4cb9fc375a353943e91949a6
Allucard79 <44382180+Allucard79@users.noreply.github.com>
2019-01-03 22:45:45 +0100

    Update Column.js

--- js/Column.js
+++ js/Column.js
@@ -1,54 +1,62 @@
 'use strict';
 
-function Column(id, name) {
+function initializeColumn(prefix, baseUrl, myHeaders) {
+  const Card = initializeCard(prefix, baseUrl, myHeaders);
+
+  function Column(id, name) {
     var self = this;
     this.id = id;
     this.name = name || 'No name given';
     this.element = generateTemplate('column-template', {
-        name: this.name,
-        id: this.id
+      name: this.name,
+      id: this.id
     });
 
     this.element.querySelector('.column').addEventListener('click', function (event) {
-        if (event.target.classList.contains('btn-delete')) {
-            self.removeColumn();
-        }
+      if (event.target.classList.contains('btn-delete')) {
+        self.removeColumn();
+      }
 
-        if (event.target.classList.contains('add-card')) {
-            var cardName = prompt("Enter the name of the card");
-            event.preventDefault();
+      if (event.target.classList.contains('add-card')) {
+        var cardName = prompt("Enter the name of the card");
+        event.preventDefault();
 
-            var data = new FormData();
-data.append('name', cardName);
-data.append('bootcamp_kanban_column_id', self.id);
+        var data = new FormData();
+        data.append('name', cardName);
+        data.append('bootcamp_kanban_column_id', self.id);
 
-fetch(prefix + baseUrl + '/card', {
-    method: 'POST',
-    headers: myHeaders,
-    body: data,
-  })
-  .then(function(res) {
-    return res.json();
-  })
-  .then(function(resp) {
-    var card = new Card(resp.id, cardName);
-    self.addCard(card);
-  });
-          }
+        fetch(prefix + baseUrl + '/card', {
+            method: 'POST',
+            headers: myHeaders,
+            body: data,
+          })
+          .then(function (res) {
+            return res.json();
+          })
+          .then(function (resp) {
+            var card = new Card(resp.id, cardName);
+            self.addCard(card);
+          });
+      }
     });
+  }
+  Column.prototype = {
+    addCard: function (card) {
+      this.element.querySelector('ul').appendChild(card.element);
+    },
+    removeColumn: function () {
+      var self = this;
+      fetch(prefix + baseUrl + '/column/' + self.id, {
+          method: 'DELETE',
+          headers: myHeaders
+        })
+        .then(function (resp) {
+          return resp.json();
+        })
+        .then(function (resp) {
+          self.element.parentNode.removeChild(self.element);
+        });
+    }
+  };
+  return Column;
 }
-    Column.prototype = {
-        addCard: function(card) {
-          this.element.querySelector('ul').appendChild(card.element);
-        },
-        removeColumn: function() {
-          var self = this;
-          fetch(prefix + baseUrl + '/column/' + self.id, { method: 'DELETE', headers: myHeaders })
-            .then(function(resp) {
-              return resp.json();
-            })
-            .then(function(resp) {
-              self.element.parentNode.removeChild(self.element);
-            });
-        }
-      };
